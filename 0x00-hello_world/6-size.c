62532ca087cdc62b0d4c2e8e3bafd4fb5ed2c646
Adesina12 <ab702810@gmail.com>
2022-06-30 18:26:46 -0700

    sdf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,8 @@ int main(void)
 	long int li;
 	long long ll;
 	float f;
-	
 	printf("Size of a char: %ld byte(s)\n", (unsigned int) sizeof(c));
 
-	
 	printf("Size of an int: %ld byte(s)\n", (unsigned int) sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", (unsigned long) sizeof(ll));

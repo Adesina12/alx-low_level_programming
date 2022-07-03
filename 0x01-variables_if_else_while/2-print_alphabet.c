98886a7f0f7524edbe7cdd60a341f47c30240474
Adesina12 <ab702810@gmail.com>
2022-07-03 04:16:36 -0700

    kjh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - entry mode
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+	char a;
+	
+	for(a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	return (0);
+}

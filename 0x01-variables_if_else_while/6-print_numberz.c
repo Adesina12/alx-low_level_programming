f2598e631ea207b2602ca63939fa86c3b80c0fe6
Adesina12 <ab702810@gmail.com>
2022-07-03 18:17:31 -0700

    kjh

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - entry mode
+ * main - Entry mode
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;	
-	for (num = '0'; num <= '9'; num++)
-	       putchar(num);
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
 	putchar('\n');
 	return (0);
 }

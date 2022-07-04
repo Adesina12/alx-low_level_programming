382fc45664cb9231290f21eae45e4d4fe0590d1c
Adesina12 <ab702810@gmail.com>
2022-07-03 17:33:45 -0700

    lkj

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry mode
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while(num < 10)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}

698d827c01e0821321fdf30c80ede93bc9101d00
Adesina12 <ab702810@gmail.com>
2022-07-06 04:36:37 -0700

    asd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * main - Entry mode
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet()
 {
 	int a;
 
@@ -14,6 +14,6 @@ void print_alphabet(void)
 }
 int main(void)
 {
-	void print_alphabet(void);
+	void print_alphabet();
 	return (0);
 }

fea50047a4798db2525788960bb9d9c5dc7d4c73
Adesina12 <ab702810@gmail.com>
2022-07-06 21:24:45 -0700

    gfd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry mode
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int w = 0;
+
+	while (w > 10)
+	{
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+	}
+}

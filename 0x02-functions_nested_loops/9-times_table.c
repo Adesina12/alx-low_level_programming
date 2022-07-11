9865c317a5bdf91932544f430dc1bcc7a965749b
Adesina12 <ab702810@gmail.com>
2022-07-11 10:55:03 -0700

    poi

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,8 +26,8 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar('0' + c / 10);
-				_putchar('0' + c % 10);
+				_putchar('0' + (c / 10));
+				_putchar('0' + (c % 10));
 			}
 
 			if (b < 9)

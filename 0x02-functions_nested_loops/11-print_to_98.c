2fc215195d756627483f6255f456420bb1304001
Adesina12 <ab702810@gmail.com>
2022-07-11 13:38:51 -0700

    asd*

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry mode
+ * @n: an integer
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n < '98')
+	{
+		for (; n <= '98'; n++)
+		{
+			_putchar(n + '0');
+			_putchar(' ');
+			_putchar(',');
+	}
+	else 
+	{
+		for (; n >= '98'; n--)
+		{
+			_putchar(n + '0');
+			_putchar(' ');
+			_putchar(',');
+		}
+	}
+	}
+}

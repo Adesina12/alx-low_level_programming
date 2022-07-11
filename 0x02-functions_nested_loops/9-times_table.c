144ccd9926662f7643cf880b691fee4b429bef4e
Adesina12 <ab702810@gmail.com>
2022-07-11 10:48:02 -0700

    kjh

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - Entry mode
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			c == a * b;
+			if (b == 0)
+			{
+				_putchar('0' + c);
+			}
+			else if (b < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + c);
+			}
+			else
+			{
+				_putchar('0' + c / 10);
+				_putchar('0' + c % 10);
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}

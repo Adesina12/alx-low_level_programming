0d5eab13bc28d19ad839d2136ad535f6ec59b736
Adesina12 <ab702810@gmail.com>
2022-07-14 13:04:22 -0700

    iuy

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef main
-#define main
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,4 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-#endif

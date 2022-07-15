4fa7b246c358320e4c98778ebb2587e9553adf1b
Adesina12 <ab702810@gmail.com>
2022-07-14 23:38:49 -0700

    iuyt

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Entry mode
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < = 9; a++)
+		_putchar(a);
+	_putchar('\n');
+}

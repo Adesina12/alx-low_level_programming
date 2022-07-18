0276aa132d4cf5f1ccca705181f1a845ec62c42c
Adesina12 <ab702810@gmail.com>
2022-07-18 07:34:40 -0700

    iyg

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,10 +8,15 @@ void print_line(int n)
 {
 	int a = 0;
 
-	while (a <= n)
+	if (n > 0)
 	{
-		_putchar('_');
-		a++;
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }

3174658d50e80b20e4976be39b461eee3e1e22d4
Adesina12 <ab702810@gmail.com>
2022-07-19 13:39:30 -0700

    jhg

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry mode
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			b = 0;
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			a++;
+		}
+	}
+	else
+		_putchar('\n');
+}

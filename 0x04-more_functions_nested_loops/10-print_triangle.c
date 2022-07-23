d0615ad49a8f64e34a2d493459058affa8c879ab
Adesina12 <ab702810@gmail.com>
2022-07-22 22:58:14 -0700

    jhg

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,12 +11,12 @@ void print_triangle(int size)
 	for (a = 0; a < size; a++)
 	{
 		for (b = 1; b < (size - a); b++)
-			putchar(' ');
-		for (b--; b < size; b++);
-		putchar(35);
+			_putchar(' ');
+		for (b--; b < size; b++)
+			_putchar(35);
 		if (a < (size - 1))
-			putchar('\n');
+			_putchar('\n');
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }

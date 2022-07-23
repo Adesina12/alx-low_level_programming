d7a0c4c9c4757bf99f2e419df3f4b08082775b07
Adesina12 <ab702810@gmail.com>
2022-07-22 22:53:01 -0700

    hgg

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h";
+/**
+ * print_triangle - Entry mode
+ * @size: an integer
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int a, b;
+	size = 5;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b < (size - a); b++)
+			putchar(' ');
+		for (b--; b < size; b++);
+		putchar(35);
+		if (a < (size - 1))
+			putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}

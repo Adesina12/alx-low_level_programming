3669d20e9257b12a5a92907300758e4f48b9f2f7
Adesina12 <ab702810@gmail.com>
2022-10-05 02:55:46 -0700

    jgfd

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Entry mode
+ * @str: a pointer
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int n = strlen(str);
+	int i;
+	if ((n%2) = 1)
+	{
+		for (i = (n - 1) / 2; i >=0; i--)
+			_putchar(str);
+		_putchar('\n');
+	}
+	else
+	 {
+		 for (i = n/2; i >=0; i--)
+			 _putchar(str);
+		 _putchar('\n');
+	 }
+}

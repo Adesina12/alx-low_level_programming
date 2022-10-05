4024ba9526adfce3ac2fbd4c7111e38426355ca7
Adesina12 <ab702810@gmail.com>
2022-10-04 23:18:50 -0700

    uyt

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /** 
  * print_rev - Entry mode
  * @s: a pointer
@@ -7,5 +8,12 @@
  */
 void print_rev(char *s)
 {
-	rev(s);
+	int n = strlen(str);
+	int i;
+
+	for (i < n - 1; i < 0; i--)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
 }

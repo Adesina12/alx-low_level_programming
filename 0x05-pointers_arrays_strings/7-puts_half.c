484020c04abe106223b5aedd83aa5765ce083eb4
Adesina12 <ab702810@gmail.com>
2022-10-05 10:33:36 -0700

    jhhg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,16 @@ void puts_half(char *str)
 {
 	int n = strlen(str);
 	int i;
+
 	if ((n % 2) == 1)
 	{
-		for (i = (n + 1)/2; i <= (n - 1); i++)
+		for (i = (n + 1) / 2; i <= (n - 1); i++)
 			_putchar(str[i]);
 		_putchar('\n');
 	}
 	else
 	 {
-		 for (i = n/2; i <=(n-1); i++)
+		 for (i = n / 2; i <= (n - 1); i++)
 			 _putchar(str[i]);
 		 _putchar('\n');
 	 }

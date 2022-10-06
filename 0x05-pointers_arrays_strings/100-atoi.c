4bb695f2bc48c69d1d3ef31769d4f93a51ba9f8f
Adesina12 <ab702810@gmail.com>
2022-10-05 23:02:18 -0700

    ugff

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - Entry mode
+ * @s: a pointer
+ * Return: atoi
+ */
+
+int _atoi(char *s)
+{
+	atoi(s);
+
+	return (0);
+}

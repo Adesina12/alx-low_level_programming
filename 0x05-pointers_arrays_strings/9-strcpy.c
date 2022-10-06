d16fabfe98aec1c2efcb21c9515a2b1c6525befc
Adesina12 <ab702810@gmail.com>
2022-10-05 22:45:37 -0700

    grds

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * strcpy - Entry mode
+ * @dest: a pointer
+ * @src: another pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return(dest);
+}

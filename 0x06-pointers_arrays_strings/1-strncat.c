0fe6162221ca59aa4d6fca22f6b2a34fe994d7c2
Adesina12 <ab702810@gmail.com>
2022-10-06 00:50:05 -0700

    jhgf

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 
 /**
- * _strncat - entry mode
- * @dest: destination
- * @src: source
- * @n: an integer
- * Return: dest
+ * _strncat -> function to append some charx
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
-
 	return (dest);
 }

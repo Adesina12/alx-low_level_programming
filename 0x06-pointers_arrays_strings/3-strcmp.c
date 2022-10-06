c2f26609faac97019e145a0f55c4f353ad5cf554
Adesina12 <ab702810@gmail.com>
2022-10-06 02:41:39 -0700

    jhgf

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Entry mode
+ * @s1: a pointer
+ * @s2: another pointer
+ * Return: s1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+
+	return (0);
+}

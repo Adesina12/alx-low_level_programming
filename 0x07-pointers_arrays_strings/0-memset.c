041fda1ed4e695bff6afc98a8c484e753d95b7e5
Adesina12 <ab702810@gmail.com>
2022-10-10 12:55:10 -0700

    jhhg

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - Entry mode
+ * @s: pointer 1
+ * @b: pointer 2
+ * @n: an integer
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset (s, b, n);
+	return (s);
+}

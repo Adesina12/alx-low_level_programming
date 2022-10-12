3019958a2168e5a9811e9a646a50d76d86d22ebb
Adesina12 <ab702810@gmail.com>
2022-10-12 06:22:05 -0700

    jygf

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strchr - entry mode
+ * @s: a pointer
+ * @c: another pointer
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+
+	return (s);
+}

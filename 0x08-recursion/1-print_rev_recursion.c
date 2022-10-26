56f1dd452ba1dcb08ce9d8df00fc2c2dc2422b7b
Adesina12 <ab702810@gmail.com>
2022-10-26 08:49:26 -0700

    ihhg

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry mode
+ * @s: a pointer
+ * Return: none
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (n == '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+}

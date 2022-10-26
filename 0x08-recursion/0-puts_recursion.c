a6e8c4d9ff964d7f7e1cf85c9aab2eb254e2fae2
Adesina12 <ab702810@gmail.com>
2022-10-26 08:39:57 -0700

    jygff

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry mode
+ * @s: a pointer
+ * Return: no return
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		putchar('\n');
+}

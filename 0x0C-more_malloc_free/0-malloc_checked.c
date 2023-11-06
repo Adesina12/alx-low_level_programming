a0a6cd6de28039e4011720e71958ae0920058362
Adesina12 <ab702810@gmail.com>
2023-11-06 08:39:46 -0800

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - Entry point
@@ -7,9 +8,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	if (b == NULL)
+	int *prt;
+
+	prt = malloc(b)
+	if (prt == NULL)
 	{
 		exit (98);
 	}
-	malloc(b);
+	return (prt);
 }
